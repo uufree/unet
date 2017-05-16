60ad86973c5c1b465ee2f17896fe27e5c9567f83
uufree <369470777@qq.com>
2017-05-16 19:08:47 +0800

    2017.05.16 19:08

---
+++ Socket.cc
@@ -0,0 +1,170 @@
+/*************************************************************************
+	> File Name: Socket.cc
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2017年03月21日 星期二 00时12分57秒
+ ************************************************************************/
+
+#include"Socket.h"
+#include<string.h>
+
+namespace unet
+{
+    namespace net
+    {
+        namespace socket
+        {
+            Socket::Socket(SocketType type_) : type(type_),
+                socketfd(socket()),
+                bit(0x00)
+            {};
+
+            Socket::Socket(const Socket& lhs) : type(lhs.type),
+                socketfd(lhs.socketfd),
+                bit(lhs.bit)
+            {};
+
+            Socket::Socket(Socket&& lhs) : type(std::move(lhs.type)),
+                socketfd(std::move(lhs.socketfd)),
+                bit(std::move(lhs.bit))
+            {};
+            
+            Socket::~Socket()
+            {
+                unet::net::socket::close(socketfd);
+            }
+            
+            int socket(int family,int type,int protocol)
+            {
+                int n = ::socket(family,type,protocol);
+                if(n  == -1)
+                    unet::handleError(errno);
+                return n;
+            }
+
+            void listen(int socketfd)
+            {
+                int n = ::listen(socketfd,COMMSIZE);
+                if(n == -1)
+                    unet::handleError(errno);
+            }
+
+            int accept(int socketfd)
+            {
+                int connectfd = ::accept(socketfd,nullptr,nullptr);
+                if(connectfd < 0)
+                    unet::handleError(errno);
+                return connectfd;
+            }
+            
+            void connect(int socketfd,InetAddress* addr)
+            {
+                sockaddr_in addr_ = addr->getSockaddr();
+                int n = ::connect(socketfd,(sockaddr*)(&addr_),static_cast<socklen_t>(sizeof(struct sockaddr_in)));
+                
+                if(n == -1)
+                    unet::handleError(errno);
+            }
+            
+            void bind(int sockfd,InetAddress* addr)
+            {
+                sockaddr_in addr_ = addr->getSockaddr();                
+                int n = ::bind(sockfd,(sockaddr*)(&addr_),static_cast<socklen_t>(sizeof(struct sockaddr_in)));
+                
+                if(n < 0)
+                    unet::handleError(errno);
+            }
+            
+            void close(int sockfd)
+            {
+                int n = ::close(sockfd);
+                
+                if(n < 0)
+                    unet::handleError(errno);
+            }
+
+            bool setKeepAlive(int socketfd)
+            {
+                int m = 1;                
+                int n = ::setsockopt(socketfd,SOL_SOCKET,SO_KEEPALIVE,&m,static_cast<socklen_t>(sizeof(int)));
+                
+                if(n == -1)
+                    unet::handleError(errno);
+                return true;
+            }
+
+            bool setNodelay(int socketfd)
+            {
+                int m = 1;
+                int n = ::setsockopt(socketfd,IPPROTO_TCP,TCP_NODELAY,&m,static_cast<socklen_t>(sizeof(int)));
+                
+                if(n == -1)
+                    unet::handleError(errno);
+                return true;
+            }
+
+            bool setNonBlockAndCloseOnExec(int socketfd)
+            {
+                int flag = ::fcntl(socketfd,F_GETFL,0);
+                flag |= O_NONBLOCK;
+                flag |= FD_CLOEXEC;
+                int n = ::fcntl(socketfd,F_SETFL,flag);
+
+                if(n == -1)
+                    unet::handleError(errno);
+                return true;
+            }
+            
+            bool setReuseAddr(int socketfd)
+            {
+                int m = 1;
+                int n = ::setsockopt(socketfd,SOL_SOCKET,SO_REUSEADDR,&m,static_cast<socklen_t>(sizeof(int)));
+                
+                if(n == -1)
+                    unet::handleError(errno);
+                return true;
+            }
+
+            bool setReusePort(int socketfd)
+            {
+                int m = 1;
+                int n = ::setsockopt(socketfd,SOL_SOCKET,SO_REUSEPORT,&m,static_cast<socklen_t>(sizeof(int)));
+                
+                if(n == -1)
+                    unet::handleError(errno);
+                return true;
+            }
+            
+            void setKeepAlive(Socket& sock)
+            {
+                setKeepAlive(sock.getFd());
+                sock.setKeepAliveBit();
+            }
+
+            void setNodelay(Socket& sock)
+            {
+                setNodelay(sock.getFd());
+                sock.setNoDelayBit();
+            }
+
+            void setNonBlockAndCloseOnExec(Socket& sock)
+            {
+                setNonBlockAndCloseOnExec(sock.getFd());
+                sock.setNonBlockAndCloseOnExecBit();
+            }
+
+            void setReuseAddr(Socket& sock)
+            {
+                setReuseAddr(sock.getFd());
+                sock.setReuseAddrBit();
+            }
+
+            void setReusePort(Socket& sock)
+            {
+                setReusePort(sock.getFd());
+                sock.setReusePortBit();
+            }
+
+        }
+    }
+}
---
+++ Socket.h
@@ -0,0 +1,130 @@
+/*************************************************************************
+	> File Name: Socket.h
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2017年02月27日 星期一 23时56分55秒
+ ************************************************************************/
+
+#ifndef _SOCKET_H
+#define _SOCKET_H
+
+#include<assert.h>
+#include"InetAddress.h"
+#include<netinet/in.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<assert.h>
+#include<netinet/tcp.h>
+#include<fcntl.h>
+#include<unistd.h>
+/*在Socket类中用8位来标志socket属性，最高的两位保留不用，从左到右依次为：
+ * used
+ * keepalive
+ * nodelay
+ * noblock and closeonexec 
+ * reuseaddr
+ * reuseport
+ */
+
+
+namespace unet
+{
+    inline void handleError(int saveErrno)
+    {   
+        char buf[128];
+        bzero(buf,128);
+        perror(strerror_r(saveErrno,buf,128));
+        exit(1);
+    }
+
+    namespace net
+    {
+        namespace socket
+        {
+            const static int COMMSIZE = 256;
+            
+            class Socket final
+            {//用RAII处理sockfd
+                enum SocketType{CONNECT,LISTEN};
+                public:
+                    explicit Socket(SocketType type_);
+                    explicit Socket(const Socket& socketfd_);
+                    explicit Socket(Socket&& socketfd);
+                    bool operator==(const Socket& lhs);
+                    ~Socket();
+                    
+                    const Socket& operator=(const Socket& lhs) = delete;
+                    const Socket& operator=(const Socket&& lhs) = delete;
+
+                    int getFd() const
+                    {  
+                        assert(socketfd >= 0);
+                        return socketfd;
+                    }
+
+                    SocketType getType() const
+                    {return type;}
+
+                    bool isUsed() const
+                    {return bit & (1 << 5);};
+                    void setUsedBit()
+                    {bit |= (1 << 5);}
+
+                    bool isKeepAlive() const
+                    {return bit & (1 << 4);};
+                    void setKeepAliveBit()
+                    {bit |= (1 << 4);}
+
+                    bool isNoDelay() const
+                    {return bit & (1 << 3);};
+                    void setNoDelayBit()
+                    {bit |= (1 << 3);}
+
+                    bool isNonBlockAndCloseOnExec() const
+                    {return bit & (1 << 2);};
+                    void setNonBlockAndCloseOnExecBit()
+                    {bit |= (1 << 2);}
+                    
+                    bool isReuseAddr() const
+                    {return bit & (1 << 1);};
+                    void setReuseAddrBit()
+                    {bit |= (1 << 1);}
+                    
+                    bool isReusePort() const
+                    {return bit & (1 << 0);};
+                    void setReusePortBit()
+                    {bit |= (1 << 0);}
+                
+                private:
+                    const SocketType type;
+                    mutable int socketfd;
+                    unsigned char bit;
+            };
+
+            int socket(int family=AF_INET,int type=SOCK_STREAM,int protocol=IPPROTO_TCP);
+            void listen(int socketfd);
+            int accept(int socketfd);
+            void connect(int socketfd,InetAddress* addr);
+            void bind(int socketfd,InetAddress* addr);
+            void close(int socketfd);
+//这五组对fd的操作不暴露给用户        
+            bool setKeepAlive(int socketfd);
+            bool setNodelay(int sockfd);
+            bool setNonBlockAndCloseOnExec(int socketfd);
+            bool setReuseAddr(int socketfd);
+            bool setReusePort(int socketfd);
+
+            void setKeepAlive(Socket& sock);
+            void setNodelay(Socket& sock);
+            void setNonBlockAndCloseOnExec(Socket& sock);
+            void setReuseAddr(Socket& sock);
+            void setReusePort(Socket& sock);
+        }
+    }
+}
+
+
+
+#endif
+
