2c7a67db0cb3ead3ed5a993d86ceff3080b88764
uufree <369470777@qq.com>
2017-07-26 17:59:07 +0800

    add lock data

---
+++ ChannelMapWithLock.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: ChannelMapWithLock.h
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2017年07月26日 星期三 17时58分32秒
+ ************************************************************************/
+
+#ifndef _CHANNELMAPWITHLOCK_H
+#define _CHANNELMAPWITHLOCK_H
+#endif
---
+++ TcpConnectionMapWithLock.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: TcpConnectionMapWithLock.h
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2017年07月26日 星期三 17时57分31秒
+ ************************************************************************/
+
+#ifndef _TCPCONNECTIONMAPWITHLOCK_H
+#define _TCPCONNECTIONMAPWITHLOCK_H
+#endif
