8b9f84f5091ac86c05d91a8f90ac7274b73d84d0
uufree <369470777@qq.com>
2018-05-20 19:31:27 +0800

    add some

---
+++ Allocator.cc
@@ -0,0 +1,252 @@
+/*************************************************************************
+	> File Name: Allocator.cc
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2018年05月20日 星期日 16时13分23秒
+ ************************************************************************/
+
+#include"Allocator.h"
+
+namespace unet
+{
+    void* SimpleAllocator::allocate(size_t bytes)
+    {
+        void* result = ::malloc(bytes);
+        if(result == NULL)
+            result = ::malloc(bytes);
+        return result;
+    }
+
+    void SimpleAllocator::deallocate(void* ptr)
+    {
+        if(ptr)
+            ::free(ptr);
+    }
+
+    void* SimpleAllocator::reallocate(void* ptr,size_t bytes)
+    {
+        if(ptr == NULL)
+            return NULL;
+        void* newPtr = ::realloc(ptr,bytes);
+        if(newPtr == NULL)
+            newPtr = ::realloc(ptr,bytes);
+        return newPtr;
+    }
+
+    Allocator::Allocator() : 
+        u_logBufferSize(0),
+        u_logSize(LOG_INIT_SIZE),
+        u_usrBufferSize(0),
+        u_usrSize(USR_INIT_SIZE),
+        u_logHead(NULL),
+        u_logTail(NULL),
+        u_usrHead(NULL),
+        u_usrTail(NULL),
+        u_logMutex(),
+        u_usrMutex()
+    {
+        init();
+    }
+
+    Allocator::Allocator(Allocator&& alloc) :
+        u_logBufferSize(alloc.u_logBufferSize),
+        u_logSize(alloc.u_logSize),
+        u_usrBufferSize(alloc.u_usrBufferSize),
+        u_usrSize(alloc.u_usrSize),
+        u_logHead(alloc.u_logHead),
+        u_logTail(alloc.u_logTail),
+        u_usrHead(alloc.u_usrHead),
+        u_usrTail(alloc.u_usrTail),
+        u_logMutex(),
+        u_usrMutex()
+    {
+        {
+            base::MutexLockGuard guard(alloc.u_logMutex);
+            alloc.u_logHead = alloc.u_logTail = NULL;
+            alloc.u_logBufferSize = 0;
+        }
+        
+        {
+            base::MutexLockGuard guard(alloc.u_usrMutex);
+            alloc.u_usrHead = alloc.u_usrTail = NULL;
+            alloc.u_usrBufferSize = 0;
+        }
+    };
+         
+    
+    Allocator& Allocator::operator=(Allocator&& alloc)
+    {
+        if(*this == alloc)
+            return *this;
+        {
+            base::MutexLockGuard guard(u_logMutex);
+            u_logBufferSize = alloc.u_logBufferSize;
+            u_logSize = alloc.u_logSize;
+            u_logHead = alloc.u_logHead;
+            u_logTail = alloc.u_logTail;
+            {
+                base::MutexLockGuard guard(alloc.u_logMutex);
+                alloc.u_logHead = alloc.u_logTail = NULL;
+                alloc.u_logBufferSize = 0;
+            }
+        }
+        {
+            base::MutexLockGuard guard(u_usrMutex);
+            u_usrBufferSize = alloc.u_usrBufferSize;
+            u_usrSize = alloc.u_usrSize;
+            u_usrHead = alloc.u_usrHead;
+            u_usrTail = alloc.u_usrTail;
+            {
+                base::MutexLockGuard guard(u_usrMutex);
+                alloc.u_usrHead = alloc.u_usrTail = NULL;
+                alloc.u_usrBufferSize = 0;
+            }
+        }
+    }
+
+    Allocator::~Allocator()
+    {
+        LogBuffer* logTemp = NULL;
+        UsrBuffer* usrTemp = NULL;
+        while(u_logHead)
+        {
+            logTemp = u_logHead;
+            u_logHead = u_logHead->u_next;
+            delete logTemp;
+        }
+
+        while(u_usrHead)
+        {
+            usrTemp = u_usrHead;
+            u_usrHead = u_usrHead->u_next;
+            delete usrTemp;
+        }
+    }
+    
+    void Allocator::init()
+    {
+        LogBuffer* head = (LogBuffer*)SimpleAllocator::allocate(sizeof(LogBuffer));
+        head->init();
+        u_logHead = head;
+        head = head->u_next;
+        for(int i=0;i<u_logSize-1;++i)
+        {
+            head = (LogBuffer*)SimpleAllocator::allocate(sizeof(LogBuffer));
+            head->init();
+            head = u_logHead->u_next;
+        }
+        u_logTail = head;
+        u_logBufferSize = u_logSize;
+       
+        UsrBuffer* haed = (UsrBuffer*)SimpleAllocator::allocate(sizeof(UsrBuffer));
+        haed->init();
+        u_usrHead = haed;
+        haed = haed->u_next;
+        for(int i=0;i<u_usrSize-1;++i)
+        {
+            haed = (UsrBuffer*)SimpleAllocator::allocate(sizeof(UsrBuffer));
+            haed->init();
+            haed = haed->u_next;
+        }
+        u_usrTail = haed;
+        u_usrBufferSize = u_usrSize;
+    }
+    
+    LogBuffer* Allocator::allocLogBuffer()
+    {
+        if(!u_logBufferSize)
+            expandLogBuffer();
+
+        LogBuffer* res = u_logHead;
+        
+        {
+            base::MutexLockGuard guard(u_logMutex);
+            u_logHead = u_logHead->u_next;
+            --u_logBufferSize;
+
+            if(u_logBufferSize == 0)
+                u_logHead = u_logTail = NULL;
+        }
+        return res;
+    }
+
+    void Allocator::deallocLogBuffer(LogBuffer* buf)
+    {
+        base::MutexLockGuard guard(u_logMutex);
+        u_logTail = buf;
+        if(!u_logBufferSize)    //NULL，需要重新调整指针位置
+            u_logHead = buf;
+        ++u_logBufferSize;
+    }
+    
+    UsrBuffer* Allocator::allocUsrBuffer()
+    {
+        if(!u_usrBufferSize)
+            expandUsrBuffer();
+
+        UsrBuffer* res = u_usrHead;
+
+        {
+            base::MutexLockGuard guard(u_usrMutex);
+            u_usrHead = u_usrHead->u_next;
+            --u_usrBufferSize;
+
+            if(u_usrBufferSize == 0)
+                u_usrHead = u_usrTail = NULL;
+        }
+        return res;
+    }
+    
+    void Allocator::deallocUsrBuffer(UsrBuffer* buf)
+    {
+        base::MutexLockGuard guard(u_usrMutex);
+        u_usrTail = buf;
+        if(!u_usrBufferSize)
+            u_usrHead = buf;
+        ++u_usrBufferSize;
+    }
+
+    void Allocator::expandLogBuffer()
+    {
+        LogBuffer* head = (LogBuffer*)SimpleAllocator::allocate(sizeof(LogBuffer));
+        head->init();
+        LogBuffer* heads = head;
+        head = head->u_next;
+        for(int i=0;i<u_logSize-1;++i)
+        {
+            head = (LogBuffer*)SimpleAllocator::allocate(sizeof(LogBuffer));
+            head->init();
+            head = head->u_next;
+        }
+        
+        {
+            base::MutexLockGuard guard(u_logMutex);
+            u_logHead = heads;
+            u_logTail = head;
+            u_logBufferSize = u_logSize;
+            u_logSize += u_logSize;
+        }
+    }
+    
+    void Allocator::expandUsrBuffer()
+    {
+        UsrBuffer* head = (UsrBuffer*)SimpleAllocator::allocate(sizeof(UsrBuffer));
+        head->init();
+        UsrBuffer* heads = head;
+        head = head->u_next;
+        for(int i=0;i<u_usrSize-1;++i)
+        {
+            head = (UsrBuffer*)SimpleAllocator::allocate(sizeof(UsrBuffer));
+            head->init();
+            head = head->u_next;
+        }
+
+        {
+            base::MutexLockGuard guard(u_usrMutex);
+            u_usrHead = heads;
+            u_usrTail = head;
+            u_usrBufferSize = u_usrSize;
+            u_usrSize += u_usrSize;
+        }
+    }
+}
---
+++ Allocator.h
@@ -0,0 +1,117 @@
+/*************************************************************************
+	> File Name: Allocator.h
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2018年05月20日 星期日 16时13分15秒
+ ************************************************************************/
+
+#ifndef _ALLOCATOR_H
+#define _ALLOCATOR_H
+
+#include<malloc.h>
+#include<list>
+
+#include"base/Mutex.h"
+
+static const int LOG_BUF_SIZE = 4096*100-24;     
+static const int USR_BUF_SIZE = 4096-24;    
+
+#define U_USRBUFFER_FREE 0x01   //in pool
+#define U_USRBUFFER_INUSE 0x02  //in use
+#define U_USRBUFFER_DEAL 0x04   //已经有足够的数据，可以被处理了
+#define U_USRBUFFER_WAIT 0x08   //数据还不够，稍后被处理
+
+#define U_LOGBUFFER_FREE 0x01   //in poll   
+#define U_LOGBUFFER_INUSE 0x02  //in use
+#define U_LOGBUFFER_FULL 0x04   //正在被写入磁盘
+
+#define LOG_INIT_SIZE 32
+#define USR_INIT_SIZE 1024
+
+namespace unet
+{
+    struct LogBuffer
+    {
+        LogBuffer* u_next;
+        void* u_data;
+        size_t u_length;
+        int u_flag;
+        char u_buf[LOG_BUF_SIZE];
+        
+        void init()
+        {
+            u_next = NULL;
+            u_data = u_buf;
+            u_length = 0;
+            u_flag = 0;
+            u_flag |= U_USRBUFFER_FREE;
+        }
+    };
+    
+    struct UsrBuffer
+    {
+        UsrBuffer* u_next;
+        void* u_data;
+        size_t u_length;
+        int u_flag;
+        char u_buf[USR_BUF_SIZE];
+        
+        void init()
+        {
+            u_next = NULL;
+            u_data = u_buf;
+            u_length = 0;
+            u_flag = 0;
+            u_flag |= U_USRBUFFER_FREE;
+        }
+    };
+
+    class SimpleAllocator final
+    {
+        public:
+            static void* allocate(size_t);
+            static void deallocate(void*);
+            static void* reallocate(void*,size_t);
+    };
+
+    
+    /*
+     * 维护16个LogBuffer和1024个UsrBuffer
+     * */
+    
+    class Allocator final
+    {
+        public:
+            explicit Allocator();
+            Allocator(const Allocator&) = delete;
+            Allocator& operator=(const Allocator&) = delete;
+            Allocator(Allocator&&);
+            Allocator& operator=(Allocator&&);
+            ~Allocator();
+            
+            bool operator==(const Allocator& alloc) const {return u_logHead==alloc.u_logHead && u_usrHead==alloc.u_usrHead;};
+
+            LogBuffer* allocLogBuffer();
+            void deallocLogBuffer(LogBuffer*);
+            UsrBuffer* allocUsrBuffer();
+            void deallocUsrBuffer(UsrBuffer*);
+            
+        private:
+            void init();
+            void expandLogBuffer();
+            void shrinkLogBuffer(){};
+            void expandUsrBuffer();
+            void shrinkUsrBuffer(){};
+
+        private:
+            int u_logBufferSize,u_logSize;
+            int u_usrBufferSize,u_usrSize;
+            LogBuffer* u_logHead,*u_logTail;    //[)
+            UsrBuffer* u_usrHead,*u_usrTail;    //[)
+            base::MutexLock u_logMutex;
+            base::MutexLock u_usrMutex;
+    };
+};
+
+
+#endif
